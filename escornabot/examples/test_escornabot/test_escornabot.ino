03ce9bd407a0f421cf67b9c1edfb8a66bd4f461a
pedro <pedroruizf@gmail.com>
2017-11-23 17:54:54 +0100

    cambios en el ejemplo .ino de la librería

--- escornabot/examples/test_escornabot/test_escornabot.ino
+++ escornabot/examples/test_escornabot/test_escornabot.ino
@@ -16,25 +16,25 @@ void loop() {
   
 	switch (mirobot.pushButton()) {  
   
-		case 3: //si pulsamos el botón delantero, se enciende led delantero, se mueve media vuelta hacia delante, y se apaga el led delantero 
+		case 3: //si pulsamos el botón delantero, se enciende led delantero, se mueve 1/4 de vuelta hacia delante, y se apaga el led delantero 
     		mirobot.ledON (3);
 		    mirobot.drive (0.25, 10);
     		mirobot.ledOFF (3);
 			break;  		
 
-		case 4://si pulsamos el botón trasero, se enciende led trasero, se mueve media vuelta hacia atrás, y se apaga el led trasero
+		case 1://si pulsamos el botón trasero, se enciende led trasero, se mueve 1/4 de vuelta hacia atrás, y se apaga el led trasero
     		mirobot.ledON (1);
     		mirobot.drive (-0.25, 10);
     		mirobot.ledOFF (1);
   			break;
 
-  		case 2://si pulsamos el botón derecho, se enciende led derecho, se mueve 1/4 de vuelta hacia la derecha, y se apaga el led derecho
+  		case 2://si pulsamos el botón derecho, se enciende led derecho, se mueve 1/8 de vuelta hacia la derecha, y se apaga el led derecho
     		mirobot.ledON (2);
     		mirobot.turn (0.125, 10);
     		mirobot.ledOFF (2);
 		    break;
 
-  	   case 4://si pulsamos el botón izquierdo, se enciende led izquierdo, se mueve 1/4 de vuelta hacia la izquierda, y se apaga el led izquierdo
+  	   case 4://si pulsamos el botón izquierdo, se enciende led izquierdo, se mueve 1/8 de vuelta hacia la izquierda, y se apaga el led izquierdo
     	 	mirobot.ledON (4);
     		mirobot.turn (-0.125, 10);
 		    mirobot.ledOFF (4);
